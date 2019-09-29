52b5f19b980b5d0f663bf21f7535e5e680dc1b25
Tim Oliver <me@timoliver.com.au>
2019-09-30 00:58:12 +0900

    Added UUID mechanism to NSURL

---
+++ TOFileSystemObserver/Categories/NSURL+TOFileSystemUUID.h
@@ -0,0 +1,31 @@
+//
+//  NSURL+TOFileSystemUUID.h
+//  TOFileSystemObserverExample
+//
+//  Created by Tim Oliver on 30/9/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ A convenience category that wraps the ability
+ to assign a special extended attribute
+ to the file, so we can uniquely track it.
+
+ Thanks to NSHipster for highlighting this technique.
+ https://nshipster.com/extended-file-attributes/
+ */
+@interface NSURL (TOFileSystemUUID)
+
+/** Returns the unique UUID value assigned to this file, or creates a new one otherwise. */
+- (NSString *)to_fileSystemUUID;
+
+/** Regardless of whether one exists or not, generates a new UUID for this item. */
+- (NSString *)to_regenerateUUID;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TOFileSystemObserver/Categories/NSURL+TOFileSystemUUID.m
@@ -0,0 +1,54 @@
+//
+//  NSURL+TOFileSystemUUID.m
+//  TOFileSystemObserverExample
+//
+//  Created by Tim Oliver on 30/9/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import "NSURL+TOFileSystemUUID.h"
+
+#import <sys/xattr.h>
+
+static NSString * const kTOFileSystemAttributeKey = @"dev.tim.fileSystemObserver.UUID";
+
+@implementation NSURL (TOFileSystemUUID)
+
+- (NSString *)to_fileSystemUUID
+{
+    const char *filePath = [self.path fileSystemRepresentation];
+    const char *keyName = kTOFileSystemAttributeKey.UTF8String;
+
+    // Allocate a buffer for the value (UUID values are always 36 characters)
+    char value[36];
+
+    // Fetch the value from disk
+    getxattr(filePath, keyName, value, 36, 0, 0);
+
+    // Convert to a string, and return if successful
+    NSString *uuid = [[NSString alloc] initWithBytes:value length:36 encoding:NSUTF8StringEncoding];
+    if (uuid.length > 0) {
+        return uuid;
+    }
+
+    return [self to_regenerateUUID];
+}
+
+- (NSString *)to_regenerateUUID
+{
+    const char *filePath = [self.path fileSystemRepresentation];
+    const char *keyName = kTOFileSystemAttributeKey.UTF8String;
+
+    // Generate a new UUID
+    NSString *uuid = [NSUUID UUID].UUIDString;
+    const char *uuidString = [uuid cStringUsingEncoding:NSUTF8StringEncoding];
+
+    // Save it to this file
+    setxattr(filePath, keyName, uuidString, strlen(uuidString), 0, 0);
+
+    return uuid;
+}
+
+
+
+@end
