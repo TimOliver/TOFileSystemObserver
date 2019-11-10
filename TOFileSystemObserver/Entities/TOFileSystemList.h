90ea13e0924ed4345a92ba25d5ee7286bfe5038d
Tim Oliver <me@timoliver.com.au>
2019-11-10 19:25:57 +0900

    Added list live tracking object

---
+++ TOFileSystemObserver/Entities/TOFileSystemList.h
@@ -0,0 +1,42 @@
+//
+//  TOFileSystemList.h
+//
+//  Copyright 2019 Timothy Oliver. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to
+//  deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+//  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ This class represents a list of files and
+ directories located within the directroy that was
+ specified.
+ 
+ It is backed by an observer object, that will ensure
+ that it is kept up-to-date with any changes that occur on
+ the file system.
+ */
+@interface TOFileSystemList : NSObject
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TOFileSystemObserver/Entities/TOFileSystemList.m
@@ -0,0 +1,13 @@
+//
+//  TOFileSystemList.m
+//  TOFileSystemObserverExample
+//
+//  Created by Tim Oliver on 10/11/19.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import "TOFileSystemList.h"
+
+@implementation TOFileSystemList
+
+@end
