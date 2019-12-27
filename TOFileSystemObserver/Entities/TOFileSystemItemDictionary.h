6734e2cc59e0515c0e4a1ee681599d8c5e70508a
Tim Oliver <me@timoliver.com.au>
2019-12-27 15:19:15 +0800

    Added thread-safe dictionary

---
+++ TOFileSystemObserver/Entities/TOFileSystemItemDictionary.h
@@ -0,0 +1,38 @@
+//
+//  TOFileSystemItemDictionary.h
+//  TOFileSystemObserverExample
+//
+//  Created by Tim Oliver on 2019/12/27.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ A thread-safe dictionary that stores the file paths
+ to items using their on-disk UUID string as the key.
+ 
+ This is used to store an in-memory graph of all of the files
+ as they were at the start of the app session, so that any
+ detected changes to the file system can be compared.
+ */
+@interface TOFileSystemItemDictionary : NSObject
+
+/** The number of items currently in the dictionary. */
+@property (nonatomic, readonly) NSUInteger count;
+
+/** Adds an item URL to the dictionary. May be called from multiple threads. */
+- (void)setItemURL:(NSURL *)itemURL forUUID:(nullable NSString *)uuid;
+
+/** Retrieves an item URL from the dictionary. May be called from multiple threads. */
+- (nullable NSURL *)itemURLForUUID:(nullable NSString *)uuid;
+
+/** Implementations for allowing dictionary style literal syntax. */
+- (void)setObject:(id)object forKeyedSubscript:(nonnull NSString *)key;
+- (nullable id)objectForKeyedSubscript:(NSString *)key;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TOFileSystemObserver/Entities/TOFileSystemItemDictionary.m
@@ -0,0 +1,73 @@
+//
+//  TOFileSystemItemDictionary.m
+//  TOFileSystemObserverExample
+//
+//  Created by Tim Oliver on 2019/12/27.
+//  Copyright © 2019 Tim Oliver. All rights reserved.
+//
+
+#import "TOFileSystemItemDictionary.h"
+
+@interface TOFileSystemItemDictionary ()
+
+/** The dictionary that holds all of the item URLs */
+@property (nonatomic, strong) NSMutableDictionary<NSString *, NSURL*> *items;
+
+/** The dispatch queue used to read and write safely to this dictionary. */
+@property (nonatomic, strong) dispatch_queue_t itemQueue;
+
+@end
+
+@implementation TOFileSystemItemDictionary
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        _items = [NSMutableDictionary dictionary];
+        _itemQueue = dispatch_queue_create("TOFileSystemObserver.itemDictionaryQueue",
+                                           DISPATCH_QUEUE_CONCURRENT);
+    }
+    
+    return self;
+}
+
+
+- (NSUInteger)count
+{
+    return self.items.count;
+}
+
+- (void)setItemURL:(NSURL *)itemURL forUUID:(nullable NSString *)uuid
+{
+    if (uuid.length == 0) { return; }
+    
+    dispatch_barrier_async(self.itemQueue, ^{
+        self.items[uuid] = itemURL;
+    });
+}
+
+/** Retrieves an item URL from the dictionary. May be called from multiple threads. */
+- (nullable NSURL *)itemURLForUUID:(NSString *)uuid
+{
+    if (uuid.length == 0) { return nil; }
+    
+    __block NSURL *itemURL = nil;
+    dispatch_sync(self.itemQueue, ^{
+        itemURL = self.items[uuid];
+    });
+    
+    return itemURL;
+}
+
+/** Implementation for allowing dictionary style literal syntax. */
+- (void)setObject:(id)object forKeyedSubscript:(nonnull NSString *)key
+{
+    [self setItemURL:object forUUID:key];
+}
+
+- (nullable id)objectForKeyedSubscript:(NSString *)key
+{
+    return [self itemURLForUUID:key];
+}
+
+@end
