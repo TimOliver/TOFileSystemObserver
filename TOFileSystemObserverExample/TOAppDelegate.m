2689504508bfb4eb1414afb0f0ac18b5589e22e7
Tim Oliver <me@timoliver.com.au>
2020-01-13 19:07:42 +0800

    Added sample file generation

--- TOFileSystemObserverExample/TOAppDelegate.m
+++ TOFileSystemObserverExample/TOAppDelegate.m
@@ -8,22 +8,53 @@
 
 #import "TOAppDelegate.h"
 #import "TOViewController.h"
-
-@interface TOAppDelegate ()
-
-@end
+#import "TOFileSystemPath.h"
 
 @implementation TOAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
 
+    [self setUpDefaultData];
+    
     TOViewController *viewController = [[TOViewController alloc] init];
     UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
     self.window.rootViewController = navigationController;
     [self.window makeKeyAndVisible];
 
     return YES;
+}
+
+- (void)setUpDefaultData
+{
+    NSURL *documentsDirectory = [TOFileSystemPath documentsDirectoryURL];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSInteger numberOfItems = [fileManager contentsOfDirectoryAtURL:documentsDirectory
+                                         includingPropertiesForKeys:nil
+                                                            options:0
+                                                              error:nil].count;
+    if (numberOfItems > 0) { return; }
+    
+    // Create 5 folders
+    for (NSInteger i = 0; i < 5; i++) {
+        NSString *folderName = [NSString stringWithFormat:@"Folder %ld", i+1];
+        NSURL *folderURL = [documentsDirectory URLByAppendingPathComponent:folderName];
+        [fileManager createDirectoryAtURL:folderURL withIntermediateDirectories:YES attributes:nil error:nil];
+    }
+    
+    // Create 5 arbitrary files
+    NSArray *sizes = @[@(5), @(12), @(33), @(15), @(20)];
+    for (NSInteger i = 0; i < sizes.count; i++) {
+        NSString *fileName = [NSString stringWithFormat:@"File-%ld.dat", i+1];
+        NSURL *fileURL = [documentsDirectory URLByAppendingPathComponent:fileName];
+        FILE *file = fopen([fileURL.path cStringUsingEncoding:NSUTF8StringEncoding], "wb");
+        
+        NSInteger byteCount = [sizes[i] intValue] * 1000000;
+        for (NSInteger j = 0; j < byteCount; j++) {
+            fwrite("0", 1, 1, file);
+        }
+        fclose(file);
+    }
 }
 
 @end
