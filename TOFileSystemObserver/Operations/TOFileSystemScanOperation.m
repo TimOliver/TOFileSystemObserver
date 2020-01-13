b976a68bc230a6a9fd173c1ac57fc5bcc97371aa
Tim Oliver <me@timoliver.com.au>
2020-01-13 20:54:06 +0800

    Updated license

--- TOFileSystemObserver/Operations/TOFileSystemScanOperation.m
+++ TOFileSystemObserver/Operations/TOFileSystemScanOperation.m
@@ -1,7 +1,7 @@
 //
 //  TOFileSystemScanOperation.m
 //
-//  Copyright 2019 Timothy Oliver. All rights reserved.
+//  Copyright 2019-2020 Timothy Oliver. All rights reserved.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to
