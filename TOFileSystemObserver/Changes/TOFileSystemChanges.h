e92aeac0a779ae5f24bae698f15b162ed321d9c1
Tim Oliver <me@timoliver.com.au>
2020-01-16 00:16:06 +0800

    Added observer-wide changes object

---
+++ TOFileSystemObserver/Changes/TOFileSystemChanges.h
@@ -0,0 +1,17 @@
+//
+//  TOFileSystemChanges.h
+//  TOFileSystemObserverExample
+//
+//  Created by Tim Oliver on 2020/01/15.
+//  Copyright © 2020 Tim Oliver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TOFileSystemChanges : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TOFileSystemObserver/Changes/TOFileSystemChanges.m
@@ -0,0 +1,13 @@
+//
+//  TOFileSystemChanges.m
+//  TOFileSystemObserverExample
+//
+//  Created by Tim Oliver on 2020/01/15.
+//  Copyright © 2020 Tim Oliver. All rights reserved.
+//
+
+#import "TOFileSystemChanges.h"
+
+@implementation TOFileSystemChanges
+
+@end
